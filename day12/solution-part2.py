cb2952189a867b4570d8c2ba169e3583ce70569e
Michael Coleman <michael.coleman404@gmail.com>
2017-12-16 20:17:33 +0100

    up to day 16

--- day12/solution-part2.py
+++ day12/solution-part2.py
@@ -12,8 +12,33 @@ for row in rows:
     connections = connections.split(', ')
     if name not in nodes:
         nodes[name] = set()
-    nodes[name].add(x) for x in connections
-    for c in connections:
-        if c not in nodes:
-            nodes[c] = set()
-        nodes[c].add(name)
+    nodes[name].update(connections)
+
+total_groups = []
+visited_nodes = set()
+
+def check_node(name):
+    visited_nodes.add(name)
+    if name == group_name:
+        return name
+    elif group_name in nodes[name]:
+        return name
+    elif any([check_node(x) for x in nodes[name] if x not in visited_nodes]):
+        return name
+    else:
+        return False
+
+while len(nodes.keys()) > 0:
+    group_name = list(nodes.keys())[0]
+    matching_groups = []
+    for name in nodes.keys():
+        visited_nodes = set()
+        membership = check_node(name)
+        if membership:
+            matching_groups.append(membership)
+    total_groups.append(matching_groups)
+    print(f"groups for {group_name}: {matching_groups}")
+    for name in matching_groups:
+        del nodes[name]
+
+print(f"Group count is {len(total_groups)}")
--- day12/solution.py
+++ day12/solution.py
@@ -1,6 +1,9 @@
 #python solution.py input.txt
 import sys
 
+#group name is a hardcoded arg that defines membership of the group
+group_name = '0'
+
 with open(sys.argv[1]) as f:
    rows = f.readlines()
 
@@ -12,8 +15,27 @@ for row in rows:
     connections = connections.split(', ')
     if name not in nodes:
         nodes[name] = set()
-    nodes[name].add(x) for x in connections
+    nodes[name].update(connections)
 
-visited_nodes = []
-for node in nodes.keys():
-    if node
+matching_groups = []
+visited_nodes = set()
+
+def check_node(name):
+    visited_nodes.add(name)
+    if name == group_name:
+        return name
+    elif group_name in nodes[name]:
+        return name
+    elif any([check_node(x) for x in nodes[name] if x not in visited_nodes]):
+        return name
+    else:
+        return False
+
+for name in nodes.keys():
+    visited_nodes = set()
+    membership = check_node(name)
+    if membership:
+        matching_groups.append(membership)
+
+print(f"Membership is {matching_groups}")
+print(f"Membership count is {len(matching_groups)}")
